29b66b9a3db84ac2e72970b0a69245e91c482d4e
Tomas Mraz <tmraz@redhat.com>
2006-09-25 14:17:32 +0000

    Import openssl-0.9.7a-cve-2006-2940.patch from dist-git
    Dr S N Henson of the OpenSSL core team and Open Network Security
    recently developed an ASN1 test suite for NISCC (www.niscc.gov.uk). When
    the test suite was run against OpenSSL two denial of service
    vulnerabilities were discovered.
     
    Certain types of public key can take disproportionate amounts of time
    to process. This could be used by an attacker in a denial of service attack.
    CVE-2006-2940 
     
    Any code which uses OpenSSL to parse ASN1 data from untrusted sources is
    affected. This includes SSL servers which enable client authentication,
    and S/MIME applications.
     
    commit d40741d34b6e74a1e79f58418b570b626a82ef93
    Author: Tomas Mraz <tmraz@redhat.com>
    Date:   Mon Oct 2 06:53:13 2006 +0000
    
        * Mon Oct  2 2006 Tomas Mraz <tmraz@redhat.com> 0.9.7a-43.15
        - CVE-2006-2940 fix was incorrect (#208744)
    
    commit c9c2d0304d4259e2e80d7892a29b6c504512250b
    Author: Tomas Mraz <tmraz@redhat.com>
    Date:   Thu Sep 28 09:11:01 2006 +0000
    
        - patch incorrectly backported due to fuzz
    
    commit c9b0a8a95556c692a25c117cf08a566e50eb5c98
    Author: Tomas Mraz <tmraz@redhat.com>
    Date:   Tue Sep 26 10:04:22 2006 +0000
    
        - enlarge the max modulus for DH and DSA
    
    commit 02d33a0c062af44b53076fd08d0dec761ee8dbac
    Author: Tomas Mraz <tmraz@redhat.com>
    Date:   Mon Sep 25 14:17:32 2006 +0000
    
        * Mon Sep 25 2006 Tomas Mraz <tmraz@redhat.com> 0.9.7a-43.12
        - fix CVE-2006-2937 - mishandled error on ASN.1 parsing (#207276)
        - fix CVE-2006-2940 - parasitic public keys DoS (#207274)
        - fix CVE-2006-3738 - buffer overflow in SSL_get_shared_ciphers (#206940)
        - fix CVE-2006-4343 - sslv2 client DoS (#206940)

--- crypto/rsa/rsa.h
+++ crypto/rsa/rsa.h
@@ -150,6 +150,11 @@ struct rsa_st
 	BN_BLINDING *blinding;
 	};
 
+#define OPENSSL_RSA_MAX_MODULUS_BITS	16384
+
+#define OPENSSL_RSA_SMALL_MODULUS_BITS	3072
+#define OPENSSL_RSA_MAX_PUBEXP_BITS	64 /* exponent limit enforced for "large" modulus only */
+
 #define RSA_3	0x3L
 #define RSA_F4	0x10001L
 
@@ -344,6 +349,7 @@ void ERR_load_RSA_strings(void);
 #define RSA_R_INVALID_MESSAGE_LENGTH			 131
 #define RSA_R_IQMP_NOT_INVERSE_OF_Q			 126
 #define RSA_R_KEY_SIZE_TOO_SMALL			 120
+#define RSA_R_MODULUS_TOO_LARGE                          105
 #define RSA_R_NULL_BEFORE_BLOCK_MISSING			 113
 #define RSA_R_N_DOES_NOT_EQUAL_P_Q			 127
 #define RSA_R_OAEP_DECODING_ERROR			 121
