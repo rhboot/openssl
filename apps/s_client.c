956ca61ecff3acea86dbd8b230384996bca21ef5
dgregor <dgregor@redhat.com>
2009-11-12 17:15:03 +0000

    Import openssl-1.0.0-beta4-default-paths.patch from dist-git
    commit 57f18752fd808bd43228c2a78ec97b460783f17a
    Author: dgregor <dgregor@redhat.com>
    Date:   Fri Nov 20 22:28:48 2009 +0000
    
        rebase to openssl-1.0.0-0.14.beta4.el6
    
    commit bb4b6777bb115b9fe3be36544b4409a8fe86e22c
    Author: dgregor <dgregor@redhat.com>
    Date:   Fri Nov 20 21:10:40 2009 +0000
    
        openssl-1.0.0-0.4.beta3.fc12
    
    commit f5f8eb04b058383973fda0a117d45ebb4f4726c7
    Author: dgregor <dgregor@redhat.com>
    Date:   Thu Nov 12 17:15:03 2009 +0000
    
        mass-import of openssl-1.0.0-0.11.beta4.fc13.src.rpm from dist-f13

--- apps/s_client.c
+++ apps/s_client.c
@@ -899,12 +899,13 @@ bad:
 	if (!set_cert_key_stuff(ctx,cert,key))
 		goto end;
 
-	if ((!SSL_CTX_load_verify_locations(ctx,CAfile,CApath)) ||
-		(!SSL_CTX_set_default_verify_paths(ctx)))
+	if (!SSL_CTX_load_verify_locations(ctx,CAfile,CApath))
 		{
-		/* BIO_printf(bio_err,"error setting default verify locations\n"); */
 		ERR_print_errors(bio_err);
-		/* goto end; */
+		}
+	if (!SSL_CTX_set_default_verify_paths(ctx))
+		{
+		ERR_print_errors(bio_err);
 		}
 
 #ifndef OPENSSL_NO_TLSEXT
