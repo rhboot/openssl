59fef93b78ec15265eb06281b24a70b7b2132980
Tomas Mraz <tmraz@redhat.com>
2011-04-04 15:30:47 +0000

    Import openssl-fips-0.9.8e-ciph-sort.patch from dist-git
    commit 2c1110be6cbe7146316fbb7cf7ce44bf27b06fe8
    Author: Tomas Mraz <tmraz@redhat.com>
    Date:   Mon Apr 4 15:30:47 2011 +0000
    
        * Mon Apr  4 2011 Tomas Mraz <tmraz@redhat.com> 0.9.8e-19
        - presort list of ciphers available in SSL (#688901)
        - accept connection in s_server even if getaddrinfo() fails (#561260)
        - point to openssl dgst for list of supported digests (#608639)
        - fix handling of future TLS versions (#599112)
        - added VeriSign Class 3 Public Primary Certification Authority - G5
          and StartCom Certification Authority certs to ca-bundle (#675671, #617856)
        - upstream fixes for the CHIL engine (#622003, #671484)
        - Resolves: rhbz#561260 rhbz#599112 rhbz#608639 rhbz#617856 rhbz#622003 rhbz#671484 rhbz#675671 rhbz#688901

--- ssl/ssl_ciph.c
+++ ssl/ssl_ciph.c
@@ -1087,6 +1087,7 @@ STACK_OF(SSL_CIPHER) *ssl_create_cipher_list(const SSL_METHOD *ssl_method,
 		sk_SSL_CIPHER_free(*cipher_list_by_id);
 	*cipher_list_by_id = tmp_cipher_list;
 	(void)sk_SSL_CIPHER_set_cmp_func(*cipher_list_by_id,ssl_cipher_ptr_id_cmp);
+	sk_SSL_CIPHER_sort(*cipher_list_by_id);
 
 	return(cipherstack);
 	}
