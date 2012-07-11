b25969c2261de6a9fe060f710f26c3832dc86dd2
Tomas Mraz <tmraz@fedoraproject.org>
2012-07-12 00:05:11 +0200

    Import openssl-1.0.1c-ipv6-apps.patch from dist-git
    commit 254f85a5c003c6687ea07695373f3770fdbe3832
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Thu Jul 12 00:05:11 2012 +0200
    
        fix s_server with new glibc when no global IPv6 address (#839031)

--- apps/s_client.c
+++ apps/s_client.c
@@ -567,7 +567,7 @@ int MAIN(int argc, char **argv)
 	int cbuf_len,cbuf_off;
 	int sbuf_len,sbuf_off;
 	fd_set readfds,writefds;
-	short port=PORT;
+	char *port_str = PORT_STR;
 	int full_log=1;
 	char *host=SSL_HOST_NAME;
 	char *cert_file=NULL,*key_file=NULL;
@@ -668,13 +668,12 @@ int MAIN(int argc, char **argv)
 		else if	(strcmp(*argv,"-port") == 0)
 			{
 			if (--argc < 1) goto bad;
-			port=atoi(*(++argv));
-			if (port == 0) goto bad;
+			port_str= *(++argv);
 			}
 		else if (strcmp(*argv,"-connect") == 0)
 			{
 			if (--argc < 1) goto bad;
-			if (!extract_host_port(*(++argv),&host,NULL,&port))
+			if (!extract_host_port(*(++argv),&host,&port_str))
 				goto bad;
 			}
 		else if	(strcmp(*argv,"-verify") == 0)
@@ -1267,7 +1266,7 @@ bad:
 
 re_start:
 
-	if (init_client(&s,host,port,socket_type) == 0)
+	if (init_client(&s,host,port_str,socket_type) == 0)
 		{
 		BIO_printf(bio_err,"connect:errno=%d\n",get_last_socket_error());
 		SHUTDOWN(s);
