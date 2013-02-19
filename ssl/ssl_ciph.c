4825a326c5dc1ff86fdc7bdc84814be5fc3a6959
Tomas Mraz <tmraz@fedoraproject.org>
2013-02-19 16:41:14 +0100

    Import openssl-1.0.1e-env-zlib.patch from dist-git
    commit 169c3a0ddb7933a77842a57afba9ff1419cce17f
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Tue Feb 19 16:41:14 2013 +0100
    
        disable ZLIB loading by default (due to CRIME attack)

--- ssl/ssl_ciph.c
+++ ssl/ssl_ciph.c
@@ -140,6 +140,8 @@
  * OTHERWISE.
  */
 
+/* for secure_getenv */
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <openssl/objects.h>
 #ifndef OPENSSL_NO_COMP
@@ -455,7 +457,7 @@ static void load_builtin_compressions(void)
 
 			MemCheck_off();
 			ssl_comp_methods=sk_SSL_COMP_new(sk_comp_cmp);
-			if (ssl_comp_methods != NULL)
+			if (ssl_comp_methods != NULL && secure_getenv("OPENSSL_DEFAULT_ZLIB") != NULL)
 				{
 				comp=(SSL_COMP *)OPENSSL_malloc(sizeof(SSL_COMP));
 				if (comp != NULL)
