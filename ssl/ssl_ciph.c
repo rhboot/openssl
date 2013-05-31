426dbe606b98aaad1d81082803181fd1a22ceed5
Tomas Mraz <tmraz@fedoraproject.org>
2013-05-31 14:18:10 +0200

    Import openssl-1.0.1e-env-zlib.patch from dist-git
    commit e7504f833c6b0e7470f0fb8511a42801453de571
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Fri May 31 14:39:13 2013 +0200
    
        Amend the changes not suitable for rebase in RHEL-6.5.
        
        Related: rhbz#924250
    
    commit 80d34d2c7fb55c1091547eb63d1c694d174a60c7
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Fri May 31 14:18:10 2013 +0200
    
        Pick up the current openssl-1.0.1e state from rhel-7.0 branch.
        
        Related: rhbz#924250

--- ssl/ssl_ciph.c
+++ ssl/ssl_ciph.c
@@ -455,7 +455,7 @@ static void load_builtin_compressions(void)
 
 			MemCheck_off();
 			ssl_comp_methods=sk_SSL_COMP_new(sk_comp_cmp);
-			if (ssl_comp_methods != NULL)
+			if (ssl_comp_methods != NULL && __secure_getenv("OPENSSL_DEFAULT_ZLIB") != NULL)
 				{
 				comp=(SSL_COMP *)OPENSSL_malloc(sizeof(SSL_COMP));
 				if (comp != NULL)
