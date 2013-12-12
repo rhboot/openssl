285ab4565069fde8f470e3a22e154547ade23805
Tomas Mraz <tmraz@fedoraproject.org>
2013-12-12 15:56:34 +0100

    Import openssl-1.0.1e-compat-symbols.patch from dist-git
    commit 89334796b3e0c79fb292981c62de3dc4ef3bc2a6
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Thu Dec 12 15:56:34 2013 +0100
    
        add back some no-op symbols that were inadvertently dropped
        
        Resolves: rhbz#1040906

--- crypto/fips/fips.c
+++ crypto/fips/fips.c
@@ -111,6 +111,12 @@ int FIPS_module_mode(void)
 	return ret;
 	}
 
+/* just a compat symbol - return NULL */
+const void *FIPS_rand_check(void)
+	{
+	return NULL;
+	}
+
 int FIPS_selftest_failed(void)
     {
     int ret = 0;
