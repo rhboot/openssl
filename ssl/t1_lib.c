a8a6bb0ade157e6a4df5c4ef00439fe3f5e9a95b
Tomas Mraz <tmraz@fedoraproject.org>
2014-09-18 16:12:49 +0200

    Import openssl-1.0.1e-dtls-ecc-ext.patch from dist-git
    From 2054eb771ea29378f90d3a77c2f4015b17de702d Mon Sep 17 00:00:00 2001
    From: "Dr. Stephen Henson" <steve@openssl.org>
    Date: Tue, 15 Jul 2014 12:20:30 +0100
    Subject: [PATCH] Add ECC extensions with DTLS.
     
    PR#3449 
    commit 337c02f4b4fe3cb02eea6413d35805ea2686f513
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Thu Sep 18 16:12:49 2014 +0200
    
        Multiple bug fixes
        
        - add support for RFC 5649 (#1119738)
        - do not pass the FIPS integrity check if the .hmac files are empty (#1128849)
        - add ECC TLS extensions to DTLS (#1119803)
        - do not send ECC ciphersuites in SSLv2 client hello (#1090955)
        - properly propagate encryption failure in BIO_f_cipher (#1072439)
        - fix CVE-2014-0224 fix that broke EAP-FAST session resumption support
        - improve documentation of ciphersuites - patch by Hubert Kario (#1108026)
        - use case insensitive comparison for servername in s_server (#1081163)
        - add support for automatic ECDH curve selection on server (#1080128)
        - FIPS mode: make the limitations on DSA, DH, and RSA keygen
          length enforced only if OPENSSL_ENFORCE_MODULUS_BITS environment
          variable is set
        Resolves: rhbz#1119738, rhbz#1128849, rhbz#1119803, rhbz#1072439, rhbz#1106409, rhbz#1090955, rhbz#1108026, rhbz#1073914, rhbz#1081163, rhbz#1080128

--- ssl/t1_lib.c
+++ ssl/t1_lib.c
@@ -438,8 +438,7 @@ unsigned char *ssl_add_clienthello_tlsext(SSL *s, unsigned char *p, unsigned cha
 #endif
 
 #ifndef OPENSSL_NO_EC
-	if (s->tlsext_ecpointformatlist != NULL &&
-	    s->version != DTLS1_VERSION)
+	if (s->tlsext_ecpointformatlist != NULL)
 		{
 		/* Add TLS extension ECPointFormats to the ClientHello message */
 		long lenmax; 
@@ -458,8 +457,7 @@ unsigned char *ssl_add_clienthello_tlsext(SSL *s, unsigned char *p, unsigned cha
 		memcpy(ret, s->tlsext_ecpointformatlist, s->tlsext_ecpointformatlist_length);
 		ret+=s->tlsext_ecpointformatlist_length;
 		}
-	if (s->tlsext_ellipticcurvelist != NULL &&
-	    s->version != DTLS1_VERSION)
+	if (s->tlsext_ellipticcurvelist != NULL)
 		{
 		/* Add TLS extension EllipticCurves to the ClientHello message */
 		long lenmax; 
@@ -703,8 +701,7 @@ unsigned char *ssl_add_serverhello_tlsext(SSL *s, unsigned char *p, unsigned cha
         }
 
 #ifndef OPENSSL_NO_EC
-	if (s->tlsext_ecpointformatlist != NULL &&
-	    s->version != DTLS1_VERSION)
+	if (s->tlsext_ecpointformatlist != NULL)
 		{
 		/* Add TLS extension ECPointFormats to the ServerHello message */
 		long lenmax; 
@@ -1017,8 +1014,7 @@ int ssl_parse_clienthello_tlsext(SSL *s, unsigned char **p, unsigned char *d, in
 #endif
 
 #ifndef OPENSSL_NO_EC
-		else if (type == TLSEXT_TYPE_ec_point_formats &&
-	             s->version != DTLS1_VERSION)
+		else if (type == TLSEXT_TYPE_ec_point_formats)
 			{
 			unsigned char *sdata = data;
 			int ecpointformatlist_length = *(sdata++);
@@ -1052,8 +1048,7 @@ int ssl_parse_clienthello_tlsext(SSL *s, unsigned char **p, unsigned char *d, in
 			fprintf(stderr,"\n");
 #endif
 			}
-		else if (type == TLSEXT_TYPE_elliptic_curves &&
-	             s->version != DTLS1_VERSION)
+		else if (type == TLSEXT_TYPE_elliptic_curves)
 			{
 			unsigned char *sdata = data;
 			int ellipticcurvelist_length = (*(sdata++) << 8);
@@ -1412,8 +1407,7 @@ int ssl_parse_serverhello_tlsext(SSL *s, unsigned char **p, unsigned char *d, in
 			}
 
 #ifndef OPENSSL_NO_EC
-		else if (type == TLSEXT_TYPE_ec_point_formats &&
-	             s->version != DTLS1_VERSION)
+		else if (type == TLSEXT_TYPE_ec_point_formats)
 			{
 			unsigned char *sdata = data;
 			int ecpointformatlist_length = *(sdata++);
