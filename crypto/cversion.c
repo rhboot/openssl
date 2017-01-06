f5c6391451f64c20b70a3a5659a207ac335adb06
Tomas Mraz <tmraz@fedoraproject.org>
2017-01-06 14:33:52 +0100

    Import openssl-1.0.2a-version.patch from dist-git
    commit acbf02518eda913c875de1cbd07f4f15c26bf667
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Fri Jan 6 14:33:52 2017 +0100
    
        rebase to latest upstream release from the 1.0.2 branch, ABI compatible
        
        Resolves: rhbz#1276310

--- crypto/cversion.c
+++ crypto/cversion.c
@@ -62,7 +62,7 @@
 # include "buildinf.h"
 #endif
 
-const char *SSLeay_version(int t)
+const char *_current_SSLeay_version(int t)
 {
     if (t == SSLEAY_VERSION)
         return OPENSSL_VERSION_TEXT;
@@ -101,7 +101,40 @@ const char *SSLeay_version(int t)
     return ("not available");
 }
 
-unsigned long SSLeay(void)
+const char *_original_SSLeay_version(int t)
+{
+    if (t == SSLEAY_VERSION)
+        return "OpenSSL 1.0.0-fips 29 Mar 2010";
+    else
+        return _current_SSLeay_version(t);
+}
+
+const char *_original101_SSLeay_version(int t)
+{
+    if (t == SSLEAY_VERSION)
+        return "OpenSSL 1.0.1e-fips 11 Feb 2013";
+    else
+        return _current_SSLeay_version(t);
+}
+
+unsigned long _original_SSLeay(void)
+{
+    return (0x10000003L);
+}
+
+unsigned long _original101_SSLeay(void)
+{
+    return (0x1000105fL);
+}
+
+unsigned long _current_SSLeay(void)
 {
     return (SSLEAY_VERSION_NUMBER);
 }
+
+__asm__(".symver _original_SSLeay,SSLeay@");
+__asm__(".symver _original_SSLeay_version,SSLeay_version@");
+__asm__(".symver _original101_SSLeay,SSLeay@OPENSSL_1.0.1");
+__asm__(".symver _original101_SSLeay_version,SSLeay_version@OPENSSL_1.0.1");
+__asm__(".symver _current_SSLeay,SSLeay@@OPENSSL_1.0.2");
+__asm__(".symver _current_SSLeay_version,SSLeay_version@@OPENSSL_1.0.2");
