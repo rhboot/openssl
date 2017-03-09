d7513506a3ff2fc2617559f55cb5e279ca992ed3
Tomas Mraz <tmraz@fedoraproject.org>
2017-03-09 18:06:03 +0100

    Import openssl-1.0.2k-starttls.patch from dist-git
    commit f0eedf5406cda34a4216912d499ffd946958573b
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Thu Mar 9 18:06:03 2017 +0100
    
        add support for additional STARTTLS protocols to s_client
        
        Resolves: rhbz#1396209

--- apps/apps.c
+++ apps/apps.c
@@ -3277,3 +3277,11 @@ int raw_write_stdout(const void *buf, int siz)
     return write(fileno_stdout(), buf, siz);
 }
 #endif
+
+void make_uppercase(char *string)
+{
+    int i;
+
+    for (i = 0; string[i] != '\0'; i++)
+        string[i] = toupper((unsigned char)string[i]);
+}
