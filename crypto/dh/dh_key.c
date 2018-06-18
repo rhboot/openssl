d0ae260519ad441ca0a00025f0b008138f3028e4
Tomas Mraz <tmraz@fedoraproject.org>
2018-06-18 15:25:21 +0200

    Import openssl-1.0.2k-cve-2018-0732.patch from dist-git
    commit 74e597090030d94832f59b00e115c61bf950a0d1
    Author: Tomas Mraz <tmraz@fedoraproject.org>
    Date:   Mon Jun 18 15:25:21 2018 +0200
    
        Multiple fixes for RHEL-7.6 erratum
        
        add S390x assembler updates
        make CA name list comparison function case sensitive (#1548401)
        fix CVE-2017-3735 - possible one byte overread with X.509 IPAdressFamily
        fix CVE-2018-0732 - large prime DH DoS of TLS client
        fix CVE-2018-0737 - RSA key generation cache timing vulnerability
        fix CVE-2018-0739 - stack overflow parsing recursive ASN.1 structure
        Resolves: rhbz#1519396, rhbz#1548401, rhbz#1534929, rhbz#1591806, rhbz#1568682, rhbz#1563289

--- crypto/dh/dh_key.c
+++ crypto/dh/dh_key.c
@@ -133,7 +133,7 @@ static int generate_key(DH *dh)
     int ok = 0;
     int generate_new_key = 0;
     unsigned l;
-    BN_CTX *ctx;
+    BN_CTX *ctx = NULL;
     BN_MONT_CTX *mont = NULL;
     BIGNUM *pub_key = NULL, *priv_key = NULL;
 
@@ -144,6 +144,11 @@ static int generate_key(DH *dh)
         return 0;
     }
 #endif
+
+    if (BN_num_bits(dh->p) > OPENSSL_DH_MAX_MODULUS_BITS) {
+        DHerr(DH_F_GENERATE_KEY, DH_R_MODULUS_TOO_LARGE);
+        return 0;
+    }
 
     ctx = BN_CTX_new();
     if (ctx == NULL)
