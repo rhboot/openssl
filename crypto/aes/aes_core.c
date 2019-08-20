73186c514781452dffc390ae049863625af439e9
Peter Jones <pjones@redhat.com>
2019-08-20 10:27:06 -0400

    Synchronize with openssl-fips-0.9.8e-usa.tar.bz2

--- crypto/aes/aes_core.c
+++ crypto/aes/aes_core.c
@@ -37,6 +37,7 @@
 
 #include <stdlib.h>
 #include <openssl/aes.h>
+#include <openssl/fips.h>
 #include "aes_locl.h"
 
 /*
@@ -630,6 +631,10 @@ int AES_set_encrypt_key(const unsigned char *userKey, const int bits,
 	u32 *rk;
    	int i = 0;
 	u32 temp;
+
+#ifdef OPENSSL_FIPS
+	FIPS_selftest_check();
+#endif
 
 	if (!userKey || !key)
 		return -1;
